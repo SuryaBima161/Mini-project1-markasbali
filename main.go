221c29ff54fb68dab0317ba70ae4383798c8982d
SuryaBima161 <bimaputra@student.unud.ac.id>
2024-02-24 18:57:35 +0800

    initial project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type DaftarBuku struct {
+	Kode_Buku      string
+	Judul_Buku     string
+	Pengarang      string
+	Penerbit       string
+	Jumlah_Halaman string
+	Tahun_Terbit   string
+}
+
+func listBuku() {
+	var buku string
+}
+
+func editBuku() {
+
+}
+func deleteBuku() {
+
+}
+
+func main() {
+
+	fmt.Println("Hello Bang")
+}
