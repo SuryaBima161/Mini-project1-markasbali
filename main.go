2a07ac2f022735d42e1f9f78b1e54fc7e3b06463
SuryaBima161 <bimaputra@student.unud.ac.id>
2024-02-24 19:07:42 +0800

    add book function

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type DaftarBuku struct {
 	Kode_Buku      string
@@ -11,8 +16,52 @@ type DaftarBuku struct {
 	Tahun_Terbit   string
 }
 
+var listOrder []DaftarBuku
+
+func TambahBuku() {
+	judulbuku := ""
+	pengarang := ""
+	// penerbit := ""
+	// jumlahhalaman := 0
+	// tahunterbit := 0
+	inputUser := bufio.NewReader(os.Stdin)
+	fmt.Println("Tambah Kode Buku")
+	fmt.Print("Silahkan Tambah List Buku :")
+	// bufio.NewReader()
+	kodebuku, err := inputUser.ReadString('\n')
+	// _, err := fmt.Scanln(&order)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	kodebuku = strings.Replace(kodebuku, "\n", " ", 1)
+
+	fmt.Println("Add table order")
+	fmt.Print("Please add your table order :")
+
+	_, err = fmt.Scanln(&judulbuku)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Add total order")
+	fmt.Print("Please add your total order :")
+	_, err = fmt.Scanln(&pengarang)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	listOrder = append(listOrder, DaftarBuku{
+		Kode_Buku:  kodebuku,
+		Judul_Buku: judulbuku,
+		Pengarang:  pengarang,
+	})
+	fmt.Println("Order Successful ")
+}
+
 func listBuku() {
-	var buku string
+	// var buku string
 }
 
 func editBuku() {
